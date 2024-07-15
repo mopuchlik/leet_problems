37207fe6dda03a6d5146457e85dedf644d62736e
Michal Opuchlik <michal.opuchlik@gmail.com>
2024-07-15 19:09:16 +0200

    za wolno ale whateve, wynik ok

---
+++ best_time_to_buy_sell.py
@@ -0,0 +1,64 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy
+# one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction.
+# If you cannot achieve any profit, return 0.
+
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because
+# you must buy before you sell.
+
+# Example 2:
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
+# %%
+
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+
+    profit = float("-inf")
+
+    for i in range(len(prices) - 2):
+
+        prices_tmp = prices[i:]
+        buy = prices_tmp[0]
+
+        for j in range(1, len(prices_tmp)):
+            sell = prices_tmp[j]
+            profit_j = sell - buy
+            if profit_j > profit:
+                profit = profit_j
+                # print(profit)
+
+    if profit < 0:
+        profit = 0
+
+    return profit
+
+
+# %%
+prices = [7, 1, 5, 3, 6, 4]
+maxProfit(prices)
+
+
+# %%
+prices = [7, 6, 4, 3, 1]
+maxProfit(prices)
+# %%
+
+prices = [1, 2]
+maxProfit(prices)
+
+# %%
