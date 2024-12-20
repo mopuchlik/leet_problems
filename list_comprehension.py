892d05904fb5502b95210885541926592efadd37
Michal Opuchlik <michal.opuchlik@gmail.com>
2024-12-20 18:23:40 +0100

    some examples in list comprehensions

--- list_comprehension.py
+++ list_comprehension.py
@@ -40,8 +40,17 @@ z = 1
 n = 2  
 res = [[a, b, c] for a in range(x + 1) for b in range(y+1) for c in range(z+1) if a + b + c <= n]
 
+# repeat 5 times
+numbers = [(1, 2, 3) for _ in range(5)]
 
+# ### with else 
+# [f(x) if y else g(x) for x in ...]
 
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate new_filenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+# Start your code here
+new_filenames = [x.replace("hpp", "h") if x.endswith("hpp") else x for x in filenames ]
 
 
 
